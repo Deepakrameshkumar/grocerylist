86b8c24c50b8afbc81bc614cd1def2cf9b27826f
Deepak <deepakrameshkumar@hotmail.com>
2025-03-24 14:05:21 +0530

    login and redirect to main page

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -6,9 +6,9 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
     const errorMessage = document.getElementById('error-message');
 
     // Simple authentication logic (for demonstration purposes only)
-    if (username === 'admin' && password === 'password') {
-        alert('Login successful!');
-        errorMessage.textContent = '';
+    if (username === 'deepak' && password === 'prinaldi') {
+        // Redirect to the dashboard page
+        window.location.href = 'dashboard.html';
     } else {
         errorMessage.textContent = 'Invalid username or password';
     }
