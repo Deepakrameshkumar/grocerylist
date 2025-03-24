e92f1c164e4ce8277cb10a85fb71da66723fc1c2
Deepak <deepakrameshkumar@hotmail.com>
2025-03-24 16:36:39 +0530

    database connected - authentication testing

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -5,11 +5,13 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
     const password = document.getElementById('password').value;
     const errorMessage = document.getElementById('error-message');
 
-    // Simple authentication logic (for demonstration purposes only)
-    if (username === 'deepak' && password === 'prinaldi') {
-        // Redirect to the dashboard page
-        window.location.href = 'dashboard.html';
-    } else {
-        errorMessage.textContent = 'Invalid username or password';
-    }
+    // Firebase Authentication
+    firebase.auth().signInWithEmailAndPassword(username, password)
+        .then((userCredential) => {
+            // Redirect to the dashboard page
+            window.location.href = 'dashboard.html';
+        })
+        .catch((error) => {
+            errorMessage.textContent = 'Invalid username or password';
+        });
 });
